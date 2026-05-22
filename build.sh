25d27470324b4284b797c344854a01ac5b979e83
hzcqmwwk <hzc17280598389@163.com>
2026-05-23 00:13:00 +0800

    创建CPU和内存

---
+++ build.sh
@@ -0,0 +1,12 @@
+# 获取当前脚本文件所在的目录
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+
+# 编译qemu
+cd $SHELL_FOLDER/qemu-8.0.2
+if [ ! -d "$SHELL_FOLDER/output/qemu" ]; then  
+./configure --prefix=$SHELL_FOLDER/output/qemu  --target-list=riscv64-softmmu --enable-gtk  --enable-virtfs --disable-gio
+fi  
+make -j16
+make install
+cd ..
+
